8c14d2b4681be732005fb29055bf4f6cec3870c4
shewit <hailu.shewit47@gmail.com>
2024-03-20 19:53:54 -0400

    done feature 4 on app.py file

--- app.py
+++ app.py
@@ -63,12 +63,15 @@ def search_movies():
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
-    return render_template('get_single_movie.html')
+    movie_pick = movie_repository.get_movie_by_id(movie_id)
+    return render_template('get_single_movie.html', movie=movie_pick)
 
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+    movie_pick = movie_repository.get_movie_by_id(movie_id)
+
+    return render_template('edit_movies_form.html', movie=movie_pick)
 
 
 @app.post('/movies/<int:movie_id>')
