bd7c93847e4f69f91eff419e106c2e8fe74b6158
Coy <coySimon01@outlook.com>
2024-03-20 19:50:50 -0400

    Added a bunch of tests

--- tests/e2e/test_edit_movie_page.py
+++ tests/e2e/test_edit_movie_page.py
@@ -5,6 +5,8 @@ from app.py.src.models import Movie
 from app.py.src.repositories.movie_repository import get_movie_repository
 from flask.testing import FlaskClient
 
+movie_repository = get_movie_repository()
+
 def test_edit_movie_page(test_app: FlaskClient):
    response = test_app.get('/movies/1/edit')
    assert response.status_code == 200
@@ -22,3 +24,52 @@ def test_edit_movie_page(test_app: FlaskClient):
    assert b"Francis Coppola" in response.data
    assert b"5" in response.data
     
+def test_invalid_movie_edit(test_app: FlaskClient):
+    data = { 
+        'title': '',
+        'director': 'Franics Coppola',
+        'rating': '5'
+    }
+
+    response = test_app.post('/movies/1', data=data, follow_redirects=True)
+
+    assert response.status_code == 400
+    assert b"Bad Request" in response.data
+    assert b"Title is required" in response.data
+
+def test_movie_not_found(test_app: FlaskClient):
+    response = test_app.get('/movies/999/edit')
+
+    assert response.status_code == 401
+    assert b"Unauthorized" in response.data
+
+def test_duplicate_movie_title(test_app: FlaskClient):
+    data = {
+        'title': 'The Godfather',
+        'director': 'Franics Coppola',
+        'rating': '5'
+    }
+
+    response = test_app.post('/movies/1', data=data, follow_redirects=True)
+
+    assert response.status_code == 409
+    assert b"Conflict" in response.data
+    assert b"A movie with the same title already exists" in response.data
+
+def test_edit_non_existent_movie(test_app: FlaskClient):
+    response = test_app.get('/movies/999/edit')
+    assert response.status_code == 404
+    assert b"Not Found" in response.data
+    assert b"Movie not found" in response.data
+
+def test_invalid_input_format(test_app: FlaskClient):
+    data = {
+        'title': 'The Godfather',
+        'director': 'Francis Coppola',
+        'rating': 'InvalidRating'
+    }
+    response = test_app.post('/movies/1', data=data, follow_redirects=True)
+
+    assert response.status_code == 400
+    assert b"Bad Request" in response.data
+    assert b"Invalid rating format" in response.data
--- tests/unit/test_update_movie.py
+++ tests/unit/test_update_movie.py
@@ -1,15 +1,33 @@
 # TODO: Feature 5
 import pytest
 from app.py import app
+from flask.testing import FlaskClient
 
 
 def test_create_movie(test_app: FlaskClient):
-    response = FlaskClient.post('/movies/1', data={"title": "New Movie Title", "director": "New Director", "rating": "5"})
+    response = test_app.post('/movies/1', data={"title": "New Movie Title", "director": "New Director", "rating": "5"})
     assert response.status_code == 302
 
 def test_update_movie(test_app: FlaskClient):
-    response_create = client.post('/movies')
+    response_create = test_app.post('/movies')
     assert response_create.status_code == 302
 
-    response = client.post('/movies/1', data={"title": "Updated Movie Title", "director": "Updated Director", "rating": "4"})
+    response = test_app.post('/movies/1', data={"title": "Updated Movie Title", "director": "Updated Director", "rating": "4"})
     assert response.status_code == 302
+
+def test_get_movie(test_app: FlaskClient):
+    response = test_app.get('/movies/1')
+    assert response.status_code == 200
+
+def test_delete_movie(test_app: FlaskClient):
+    response = test_app.delete('/movies/1')
+    assert response.status_code == 204
+
+def test_list_movies(test_app: FlaskClient):
+    response = test_app.get('/movies')
+    assert response.status_code == 200
+
+def test_invalid_create_movie(test_app: FlaskClient):
+    response = test_app.post('/movies', data={"title": "", "director": "Invalid Director", "rating": "Invalid Rating"})
+    assert response.status_code == 400
+
